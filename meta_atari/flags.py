33619ffcc1022c82560d3ac6d80f7f6e9bd57856
Ioana Chelu <ioana.chelu28@gmail.com>
2017-02-18 10:02:35 +0200

    Refactorization + added updated to target network every 40 000 frames for A3C and train tot 5 before each updated

---
+++ meta_atari/flags.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+# Basic model parameters.
+tf.app.flags.DEFINE_string('game', 'independent',
+                           """Bandit experiment type to be run""")
+tf.app.flags.DEFINE_boolean('resume', False,
+                            """Resume training from latest checkpoint""")
+tf.app.flags.DEFINE_boolean('train', True,
+                            """Whether to train or test""")
+tf.app.flags.DEFINE_string('checkpoint_dir', './models',
+                           """Directory where to save model checkpoints.""")
+tf.app.flags.DEFINE_string('summaries_dir', './summaries',
+                           """Directory where to write event logs""")
+tf.app.flags.DEFINE_string('frames_dir', './frames',
+                           """Directory where to write event gifs""")
+tf.app.flags.DEFINE_string('frames_test_dir', './frames_test',
+                           """Directory where to write test event gifs""")
+tf.app.flags.DEFINE_integer('summary_interval', 5, """Number of episodes of interval between summary saves""")
+tf.app.flags.DEFINE_integer('checkpoint_interval', 500, """Number of episodes of interval between checkpoint saves""")
+tf.app.flags.DEFINE_integer('frames_interval', 100, """Number of episodes of interval between frames saves""")
+tf.app.flags.DEFINE_integer('nb_actions', 2, """Number of actions to take""")
+tf.app.flags.DEFINE_float('gamma', 0.6, """Gamma value""")
+tf.app.flags.DEFINE_float('lr', 1e-2, """Learning rate""")
+tf.app.flags.DEFINE_float('beta_v', 0.05, """Coefficient of value function loss""")
+tf.app.flags.DEFINE_integer('max_nb_episodes_train', 20000, """Max number of episodes of training time""")
+tf.app.flags.DEFINE_float('gradient_clip_value', 50.0, """gradient_clip_value""")
+tf.app.flags.DEFINE_integer('nb_test_episodes', 150, """Test episodes""")
---
+++ meta_bandits/run.py
@@ -0,0 +1,84 @@
+import threading
+
+import tensorflow as tf
+from agent import Worker
+from envs.bandit_envs import TwoArms, ElevenArms
+from network import AC_Network
+import flags
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def recreate_directory_structure():
+    if not tf.gfile.Exists(FLAGS.checkpoint_dir):
+        tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
+    else:
+        if not FLAGS.resume and FLAGS.train:
+            tf.gfile.DeleteRecursively(FLAGS.checkpoint_dir)
+            tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
+
+    if not tf.gfile.Exists(FLAGS.frames_dir):
+        tf.gfile.MakeDirs(FLAGS.frames_dir)
+    else:
+        if not FLAGS.resume and FLAGS.train:
+            tf.gfile.DeleteRecursively(FLAGS.frames_dir)
+            tf.gfile.MakeDirs(FLAGS.frames_dir)
+
+    if not tf.gfile.Exists(FLAGS.frames_test_dir):
+        tf.gfile.MakeDirs(FLAGS.frames_test_dir)
+    else:
+        if FLAGS.resume and not FLAGS.train:
+            tf.gfile.DeleteRecursively(FLAGS.frames_test_dir)
+            tf.gfile.MakeDirs(FLAGS.frames_test_dir)
+
+    if not tf.gfile.Exists(FLAGS.summaries_dir):
+        tf.gfile.MakeDirs(FLAGS.summaries_dir)
+    else:
+        if not FLAGS.resume and FLAGS.train:
+            tf.gfile.DeleteRecursively(FLAGS.summaries_dir)
+            tf.gfile.MakeDirs(FLAGS.summaries_dir)
+
+
+def run():
+    recreate_directory_structure()
+    tf.reset_default_graph()
+
+    with tf.device("/cpu:0"):
+        global_step = tf.Variable(0, dtype=tf.int32, name='global_episodes', trainable=False)
+        optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.lr)
+        global_network = AC_Network('global', None)
+
+        num_workers = 1
+        workers = []
+        envs = []
+        for i in range(num_workers):
+            if FLAGS.game == '11arms':
+                this_env = ElevenArms()
+            else:
+                this_env = TwoArms(FLAGS.game)
+            envs.append(this_env)
+
+        for i in range(num_workers):
+            workers.append(Worker(envs[i], i, optimizer, global_step))
+        saver = tf.train.Saver(max_to_keep=5)
+
+    with tf.Session() as sess:
+        coord = tf.train.Coordinator()
+        if FLAGS.resume:
+            ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+            print("Loading Model from {}".format(ckpt.model_checkpoint_path))
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            sess.run(tf.global_variables_initializer())
+
+        worker_threads = []
+        for worker in workers:
+            worker_play = lambda: worker.play(sess, coord, saver)
+            thread = threading.Thread(target=(worker_play))
+            thread.start()
+            worker_threads.append(thread)
+        coord.join(worker_threads)
+
+
+if __name__ == '__main__':
+    run()
