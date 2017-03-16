2fe9601bd7f0d5b3e45db93706f5563c105ea5ee
Ioana Chelu <ioana.chelu28@gmail.com>
2017-03-16 13:18:02 +0200

    New test for meta-mdp for noMeta with gen adv

--- async/flags.py
+++ async/flags.py
@@ -15,15 +15,15 @@ tf.app.flags.DEFINE_string('summaries_dir', './summaries',
                            """Directory where to write event logs""")
 tf.app.flags.DEFINE_string('experiments_dir', './experiments',
                            """Directory where to write event experiments""")
-tf.app.flags.DEFINE_integer('summary_interval', 500, """Number of episodes of interval between summary saves""")
-tf.app.flags.DEFINE_integer('test_performance_interval', 500,
+tf.app.flags.DEFINE_integer('summary_interval', 100, """Number of episodes of interval between summary saves""")
+tf.app.flags.DEFINE_integer('test_performance_interval', 100,
                             """Number of episodes of interval between testing reward performance""")
-tf.app.flags.DEFINE_integer('checkpoint_interval', 2000, """Number of episodes of interval between checkpoint saves""")
+tf.app.flags.DEFINE_integer('checkpoint_interval', 100, """Number of episodes of interval between checkpoint saves""")
 tf.app.flags.DEFINE_integer('nb_concurrent', 4, """Number of concurrent threads""")
 tf.app.flags.DEFINE_integer('max_episode_buffer_size', 5, """Buffer size between train updates""")
 tf.app.flags.DEFINE_integer('agent_history_length', 4, """Number of frames that makes every state""")
-tf.app.flags.DEFINE_integer('resized_width', 64, """Resized width of each frame""")
-tf.app.flags.DEFINE_integer('resized_height', 64, """Resized height of each frame""")
+tf.app.flags.DEFINE_integer('resized_width', 84, """Resized width of each frame""")
+tf.app.flags.DEFINE_integer('resized_height', 84, """Resized height of each frame""")
 tf.app.flags.DEFINE_float('gamma', 0.99, """Gamma value""")
 tf.app.flags.DEFINE_float('lr', 0.0007, """Learning rate""")
 tf.app.flags.DEFINE_float('beta_v', 0.25, """Coefficient of value function loss""")
@@ -45,7 +45,7 @@ tf.app.flags.DEFINE_boolean('lstm', False,
                             """Whether to use lstm or not""")
 tf.app.flags.DEFINE_boolean('gen_adv', True,
                             """Whether to use generalized advantage estimator or not""")
-tf.flags.DEFINE_integer("eval_every", 500, "Evaluate the policy every N seconds")
+tf.flags.DEFINE_integer("eval_every", 100, "Evaluate the policy every N seconds")
 tf.app.flags.DEFINE_boolean('meta', False,
                             """Whether to use meta-learning or not""")
 tf.app.flags.DEFINE_boolean('verbose', False,
