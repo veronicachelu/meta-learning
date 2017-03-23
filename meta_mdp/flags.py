e3f56817cb5ba9c75243f9fc98bc7149405a9a3f
Ioana Chelu <ioana.chelu28@gmail.com>
2017-03-23 13:33:08 +0200

    merge

--- meta_mdp/flags.py
+++ meta_mdp/flags.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 
 # Basic model parameters.
-tf.app.flags.DEFINE_string('game', 'Gridworld-x10-v0',
+tf.app.flags.DEFINE_string('game', 'Gridworld-v0',
                            """Bandit experiment type to be run""")
-tf.app.flags.DEFINE_string('model_name', "MetaGenAdvBeta_steps_200_V_0.25_BetaE_0.05_LargeGrid", """Name of the model""")
+tf.app.flags.DEFINE_string('model_name', "IntelligentAgentBetaV_0.25_BetaE_0.05", """Name of the model""")
 tf.app.flags.DEFINE_integer('game_size', 10, """Dimension of the gridworld""")
 tf.app.flags.DEFINE_integer('game_channels', 3, """Nb of channels for each frame - rgb = 3""")
-tf.app.flags.DEFINE_boolean('resume', False,
+tf.app.flags.DEFINE_boolean('resume', True,
                             """Resume training from latest checkpoint""")
-tf.app.flags.DEFINE_boolean('train', True,
+tf.app.flags.DEFINE_boolean('train', False,
                             """Whether to train or test""")
 tf.app.flags.DEFINE_boolean('show_training', False,
                             """Show windows with workers training""")
