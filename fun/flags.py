f2dd316f5f74ecb5374d82f7869dd208d9bf60f2
ioanachelu <ioana.chelu28@gmail.com>
2017-05-10 18:59:09 +0300

    updates from remote luna

--- fun/flags.py
+++ fun/flags.py
@@ -6,7 +6,7 @@ tf.app.flags.DEFINE_string('game', 'Gridworld-x10-v2',
 tf.app.flags.DEFINE_string('model_name', "FUN", """Name of the model""")
 tf.app.flags.DEFINE_integer('game_size', 10, """Dimension of the gridworld""")
 tf.app.flags.DEFINE_integer('game_channels', 3, """Nb of channels for each frame - rgb = 3""")
-tf.app.flags.DEFINE_boolean('resume', False,
+tf.app.flags.DEFINE_boolean('resume', True,
                             """Resume training from latest checkpoint""")
 tf.app.flags.DEFINE_boolean('train', True,
                             """Whether to train or test""")
