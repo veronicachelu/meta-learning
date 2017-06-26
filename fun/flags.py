607d03fb7862e74f6d1aa35b7af75fb396dd93f8
ioanachelu <ioana.chelu28@gmail.com>
2017-06-26 15:40:21 +0300

    parameters for meta learning

--- fun/flags.py
+++ fun/flags.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
 
 # Basic model parameters.
-tf.app.flags.DEFINE_string('game', 'Gridworld-v1',
+tf.app.flags.DEFINE_string('game', 'Gridworld-v0',
                            """Bandit experiment type to be run""")
 tf.app.flags.DEFINE_string('model_name', "FUN", """Name of the model""")
 tf.app.flags.DEFINE_integer('game_size', 5, """Dimension of the gridworld""")
 tf.app.flags.DEFINE_integer('game_channels', 3, """Nb of channels for each frame - rgb = 3""")
 tf.app.flags.DEFINE_boolean('meta', True, "use meta")
-tf.app.flags.DEFINE_boolean('resume', True,
+tf.app.flags.DEFINE_boolean('resume', False,
                             """Resume training from latest checkpoint""")
-tf.app.flags.DEFINE_boolean('train', False,
+tf.app.flags.DEFINE_boolean('train', True,
                             """Whether to train or test""")
 tf.app.flags.DEFINE_boolean('show_training', False,
                             """Show windows with workers training""")
@@ -23,14 +23,14 @@ tf.app.flags.DEFINE_string('test_experiments_dir', './test_experiments',
                            """Directory where to write event test experiments""")
 tf.app.flags.DEFINE_string('frames_dir', './frames',
                            """Directory where to write event gifs""")
-tf.app.flags.DEFINE_boolean('monitor', True,
+tf.app.flags.DEFINE_boolean('monitor', False,
                             """Monitor test with gym monitor""")
 tf.app.flags.DEFINE_integer('summary_interval', 1000, """Number of episodes of interval between summary saves""")
 tf.app.flags.DEFINE_integer('checkpoint_interval', 1000, """Number of episodes of interval between checkpoint saves""")
 tf.app.flags.DEFINE_integer('nb_actions', 4, """Number of actions to take""")
 tf.app.flags.DEFINE_integer('nb_concurrent', 4, """Number of concurrent threads""")
 tf.app.flags.DEFINE_integer('explore_steps', 90000, """explore_steps""")
-tf.app.flags.DEFINE_float('initial_random_goal_prob', 0, """initial_random_goal_prob""")
+tf.app.flags.DEFINE_float('initial_random_goal_prob', 0.05, """initial_random_goal_prob""")
 tf.app.flags.DEFINE_float('final_random_goal_prob', 0, """final_random_goal_prob""")
 tf.app.flags.DEFINE_float('w_gamma', 0.99, """Gamma value""")
 tf.app.flags.DEFINE_float('m_gamma', 0.99, """Gamma value""")
@@ -43,6 +43,6 @@ tf.app.flags.DEFINE_float('gradient_clip_value', 50.0, """gradient_clip_value"""
 tf.app.flags.DEFINE_integer('nb_test_episodes', 150, """Test episodes""")
 tf.app.flags.DEFINE_integer('BTT_length', 100, 'BTT length')
 tf.app.flags.DEFINE_integer('hidden_dim', 48, 'the size of all the hidden layers')
-tf.app.flags.DEFINE_integer('manager_horizon', 5, """manager_horizon = r = c""")
+tf.app.flags.DEFINE_integer('manager_horizon', 2, """manager_horizon = r = c""")
 tf.app.flags.DEFINE_integer('goal_embedding_size', 8, """goal_embedding_size""")
 tf.app.flags.DEFINE_integer('alpha', 0.5, """regulates the influence of the intrinsic reward on the workers total reward""")
